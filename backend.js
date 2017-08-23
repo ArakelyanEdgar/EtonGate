b6145174b07086f3fda948c192703e48f05d919b
lambo4bkfast <edgararakelyan123@gmail.com>
2017-08-22 23:52:29 -0700

    a

---
+++ backend.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.set("port", (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname+"/views")))
+app.use(express.static(path.join(__dirname+"/styles")))
+app.use(express.static(path.join(__dirname+"/javascript")))
+
+app.route("/")
+    .get(function(req, res, next){
+        res.sendFile(path.join(__dirname + "/views/main.html"))
+    })
+
+app.use(function(err, res, res, next){
+    console.error(err.stack)
+    res.status(500).send("Something broke!")
+})
+
+app.use(function(req, res, next){
+    res.status(404);
+    res.sendfile(path.join(__dirname+"/views/status404.html"))
+})
